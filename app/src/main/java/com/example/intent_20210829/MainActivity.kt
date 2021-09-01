ab1904c6e3b07bb66118e7eeb2dfe454e37f8d31
gara-age <minseo00515@naver.com>
2021-09-01 23:27:16 +0900

    startAcitivityForResult - 닉네임을 받으러 간다고 명시

--- app/src/main/java/com/example/intent_20210829/MainActivity.kt
+++ app/src/main/java/com/example/intent_20210829/MainActivity.kt
@@ -6,9 +6,26 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CODE_FOR_NICKNAME = 1004
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNicknameBtn.setOnClickListener {
+
+//            닉네임 변경 화면으로 이동
+//            단순히 이동 X 새 닉네임을 받아오기 위한 왕복 이동
+
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
+
+//            데이터를 받아오기(닉네임) 위한 왕복 이동 명시
+            startActivityForResult(myIntent , REQUEST_CODE_FOR_NICKNAME)
+        }
+
+
+
 
         sendMessageBtn.setOnClickListener {
 //           메세지 보내기가 눌리면?
